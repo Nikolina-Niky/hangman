53fec8c4c67d1f06c811a9119d1ade2374b1ad9f
Nikolina <nikolina.okun@gmail.com>
2024-05-14 00:07:32 +0200

    fix typo

--- src/utils/helperFunctions.test.ts
+++ src/utils/helperFunctions.test.ts
@@ -7,7 +7,7 @@ describe("calculateScore", () => {
     expect(sore1).toBeGreaterThanOrEqual(sore2);
   });
 
-  test("result with same number of errors and higher number of unique letters i ranged higher", () => {
+  test("result with same number of errors and higher number of unique letters is ranged higher", () => {
     const sore1 = calculateScore(54, 17, 2, 65956);
     const sore2 = calculateScore(68, 20, 2, 65956);
     expect(sore2).toBeGreaterThanOrEqual(sore1);
